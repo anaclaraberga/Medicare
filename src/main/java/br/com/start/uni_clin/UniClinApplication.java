cdfca6cad45304abac8ddde972a0e74c0bc674c4
Ana Clara <ana4ew.br@gmail.com>
2024-12-11 21:47:04 -0300

    Created: Project Files with Spring Boot and Maven

---
+++ src/main/java/br/com/start/uni_clin/UniClinApplication.java
@@ -0,0 +1,13 @@
+package br.com.start.uni_clin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniClinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniClinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/start/uni_clin/UniClinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.start.uni_clin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniClinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
