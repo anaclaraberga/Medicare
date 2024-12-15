4296b5d1cc2c169eb10f5372e7bf0fb72c30160c
Ana Clara <ana4ew.br@gmail.com>
2024-12-15 15:32:32 -0300

    Created: Doctor Entity

---
+++ src/main/java/br/com/start/uni_clin/entities/Doctor.java
@@ -0,0 +1,22 @@
+package br.com.start.uni_clin.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "doctor")
+@Data
+public class Doctor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String crm;
+}
