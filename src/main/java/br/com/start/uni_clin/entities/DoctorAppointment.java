aeccc87106ab90cd08966c392fe1984ef16b2425
Ana Clara <ana4ew.br@gmail.com>
2024-12-15 22:22:33 -0300

    Added: Doctor Appointment Entity

--- src/main/java/br/com/start/uni_clin/entities/DoctorAppointment.java
+++ src/main/java/br/com/start/uni_clin/entities/DoctorAppointment.java
@@ -3,6 +3,11 @@ package br.com.start.uni_clin.entities;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -11,8 +16,12 @@ import lombok.Data;
 @Data
 public class DoctorAppointment {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String patient;
     private LocalDateTime date;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
     private Doctor doctor;
 }
