bb404270d5c13f82b3e43bb3f7bd36c4cd26e343
Ana Clara <ana4ew.br@gmail.com>
2024-12-15 22:29:36 -0300

    Added: Doctor Appointment Request

---
+++ src/main/java/br/com/start/uni_clin/dtos/request/DoctorAppointmentRequest.java
@@ -0,0 +1,12 @@
+package br.com.start.uni_clin.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DoctorAppointmentRequest {
+    
+    private String patient;
+    private String doctor;
+}
