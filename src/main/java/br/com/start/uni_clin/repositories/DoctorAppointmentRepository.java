c8e410f2ef51a72a3ed8090196351b6f60129530
Ana Clara <ana4ew.br@gmail.com>
2024-12-26 16:02:44 -0300

    Added: Repository Annotation

--- src/main/java/br/com/start/uni_clin/repositories/DoctorAppointmentRepository.java
+++ src/main/java/br/com/start/uni_clin/repositories/DoctorAppointmentRepository.java
@@ -1,9 +1,11 @@
 package br.com.start.uni_clin.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.start.uni_clin.entities.DoctorAppointment;
 
+@Repository
 public interface DoctorAppointmentRepository extends JpaRepository<DoctorAppointment, Long>{
     
 }
