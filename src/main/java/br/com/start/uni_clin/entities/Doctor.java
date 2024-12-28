6c7be85c6994df10e0418a43fbd2e640e1a50c68
Ana Clara <ana4ew.br@gmail.com>
2024-12-28 15:16:59 -0300

    Fixed: Annotation

--- src/main/java/br/com/start/uni_clin/entities/Doctor.java
+++ src/main/java/br/com/start/uni_clin/entities/Doctor.java
@@ -6,11 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "doctor")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Doctor {
 
     @Id
