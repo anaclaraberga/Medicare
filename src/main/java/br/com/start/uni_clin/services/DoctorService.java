8c646357777c44f8143a4c0607408370ce99177c
Ana Clara <ana4ew.br@gmail.com>
2025-01-01 11:54:34 -0300

    Created: Update, find all and delete service

--- src/main/java/br/com/start/uni_clin/services/DoctorService.java
+++ src/main/java/br/com/start/uni_clin/services/DoctorService.java
@@ -1,9 +1,12 @@
 package br.com.start.uni_clin.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.start.uni_clin.dtos.request.DoctorRequest;
@@ -28,5 +31,30 @@ public class DoctorService {
         Optional<Doctor> doctor = this.doctorRepository.findById(id);
 
         return doctor.orElseThrow(() -> new ObjectNotFoundException("Doutor não encontrado! Id: " + id + "Tipo: " + Doctor.class.getName(), doctor));
+    }
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public ResponseEntity<Doctor> updateDoctorById(DoctorRequest dto, Long id) {
+        return doctorRepository.findById(id)
+        .map( update -> {
+            update.setCrm(dto.getCrm());
+            update.setEmail(dto.getEmail());
+            update.setName(dto.getName());
+            update.setPhone(dto.getPhone());
+
+            Doctor updated = doctorRepository.save(update);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Object> deleteDoctorById(Long id) {
+        return doctorRepository.findById(id)
+        .map( delete -> {
+            doctorRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
