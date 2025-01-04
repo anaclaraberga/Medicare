c2e3c01b9570371c84dc397acd3975b14e8e1d44
Ana Clara <ana4ew.br@gmail.com>
2025-01-04 09:24:25 -0300

    Created: DoctorAppointment to Request Function

--- src/main/java/br/com/start/uni_clin/entities/DoctorAppointment.java
+++ src/main/java/br/com/start/uni_clin/entities/DoctorAppointment.java
@@ -2,11 +2,13 @@ package br.com.start.uni_clin.entities;
 
 import java.time.LocalDateTime;
 
+import br.com.start.uni_clin.dtos.request.DoctorAppointmentRequest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -23,5 +25,10 @@ public class DoctorAppointment {
     private LocalDateTime date;
     
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", referencedColumnName = "id")
     private Doctor doctor;
+
+    public DoctorAppointment(DoctorAppointmentRequest dto) {
+        this.patient = dto.getPatient();
+    }
 }
