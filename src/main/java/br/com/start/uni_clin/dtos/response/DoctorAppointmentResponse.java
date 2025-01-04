36fdd6fa3e79a322d800aa18cfa38a36bc53bd45
Ana Clara <ana4ew.br@gmail.com>
2025-01-04 09:25:50 -0300

    Created: DoctorAppointment to Response Function

--- src/main/java/br/com/start/uni_clin/dtos/response/DoctorAppointmentResponse.java
+++ src/main/java/br/com/start/uni_clin/dtos/response/DoctorAppointmentResponse.java
@@ -2,6 +2,7 @@ package br.com.start.uni_clin.dtos.response;
 
 import java.time.LocalDateTime;
 
+import br.com.start.uni_clin.entities.DoctorAppointment;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +13,12 @@ public class DoctorAppointmentResponse {
     private Long id;
     private String patient;
     private LocalDateTime date;
-    private String doctor;
+    private Long doctor;
+
+    public DoctorAppointmentResponse(DoctorAppointment dto) {
+        this.id = dto.getId();
+        this.patient = dto.getPatient();
+        this.date = dto.getDate();
+        this.doctor = dto.getDoctor().getId();
+    }
 }
