477f37fbef81bd12a52c1fbce199c4d345f9dbea
Ana Clara <ana4ew.br@gmail.com>
2025-01-09 20:16:48 -0300

    Created: Find by id and get all

--- src/main/java/br/com/start/uni_clin/controllers/PatientController.java
+++ src/main/java/br/com/start/uni_clin/controllers/PatientController.java
@@ -1,9 +1,13 @@
 package br.com.start.uni_clin.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +40,22 @@ public class PatientController {
         Patient patient = patientService.createPatient(dto);
 
         return ResponseEntity.status(201).body(new PatientResponse(patient));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> findPatientById(@PathVariable Long id) {
+
+        Patient entity = this.patientService.findPatientById(id);
+
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Patient> getAllPatients() {
+        
+        List<Patient> patient = patientService.getAllPatients();
+
+        return patient;
     }
 }
