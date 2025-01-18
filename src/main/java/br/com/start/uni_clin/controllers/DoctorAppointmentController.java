a41466ed76bed17b754acfa3fb742f3379a35eb4
Ana Clara <ana4ew.br@gmail.com>
2025-01-17 22:15:49 -0300

    Feature: Doctor Appointment Controller CRUD

--- src/main/java/br/com/start/uni_clin/controllers/DoctorAppointmentController.java
+++ src/main/java/br/com/start/uni_clin/controllers/DoctorAppointmentController.java
@@ -1,10 +1,16 @@
 package br.com.start.uni_clin.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -35,5 +41,35 @@ public class DoctorAppointmentController {
         DoctorAppointment doctorAppointment = doctorAppointmentService.createDoctorAppointment(dto);
 
         return ResponseEntity.status(201).body(new DoctorAppointmentResponse(doctorAppointment));
-    }   
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorAppointment> findDoctorAppointmentById(@PathVariable Long id) {
+
+        DoctorAppointment entity = this.doctorAppointmentService.findDoctorAppointmentById(id);
+
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<DoctorAppointment> findAllDoctorsAppointment() {
+
+        List<DoctorAppointment> doctorAppointments = doctorAppointmentService.getAllDoctorAppointments();
+
+        return doctorAppointments;
+    } 
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<DoctorAppointment> updateDoctorAppointmentById(@PathVariable (value = "id") Long id, @RequestBody DoctorAppointmentRequest dto) {
+
+        return doctorAppointmentService.updateDoctorAppointmentById(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> updateDoctorAppointmentById(@PathVariable("id") Long id) {
+
+        return doctorAppointmentService.deleteDoctorAppointmentById(id);
+    }
 }
