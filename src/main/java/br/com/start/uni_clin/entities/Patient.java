299ab2cee23a68db654383f422921aafb6d3a14d
Ana Clara <ana4ew.br@gmail.com>
2025-01-17 22:17:11 -0300

    Fixed issue: Bad contact with hibernate

--- src/main/java/br/com/start/uni_clin/entities/Patient.java
+++ src/main/java/br/com/start/uni_clin/entities/Patient.java
@@ -7,11 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "patient")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Patient {
 
     @Id
