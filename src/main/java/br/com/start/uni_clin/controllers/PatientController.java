0189a2a9b48723437c1ac61264814b6a1fecdded
Ana Clara <ana4ew.br@gmail.com>
2025-01-17 22:16:08 -0300

    Feature: Patient  Controller CRUD

--- src/main/java/br/com/start/uni_clin/controllers/PatientController.java
+++ src/main/java/br/com/start/uni_clin/controllers/PatientController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -57,5 +59,18 @@ public class PatientController {
         List<Patient> patient = patientService.getAllPatients();
 
         return patient;
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Patient> updatePatientById(@PathVariable(value = "id") Long id, @RequestBody PatientRequest dto) {
+
+        return patientService.updatePatientById(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletePatientById(@PathVariable("id") Long id) {
+
+        return patientService.deletePatientById(id);
     }
 }
