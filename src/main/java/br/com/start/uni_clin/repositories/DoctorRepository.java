23ddbff5b4c6f3b72e8735c4b5a38b6ddb9e0b91
Ana Clara <ana4ew.br@gmail.com>
2025-01-19 18:14:15 -0300

    Inserted: New SQL query

--- src/main/java/br/com/start/uni_clin/repositories/DoctorRepository.java
+++ src/main/java/br/com/start/uni_clin/repositories/DoctorRepository.java
@@ -1,6 +1,9 @@
 package br.com.start.uni_clin.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.start.uni_clin.entities.Doctor;
@@ -8,4 +11,8 @@ import br.com.start.uni_clin.entities.Doctor;
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long>{
     
+    @Query("SELECT d FROM doctor d\n" + //
+                "WHERE d.name LIKE '%:name%'\n" + //
+                    "OR d.crm LIKE '%:crm%';")
+    Optional<Doctor> findByNameOrCrm(String name, String crm);
 }
