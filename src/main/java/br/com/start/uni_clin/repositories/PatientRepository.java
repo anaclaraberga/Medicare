26a0149b14e2b015746f0b1c9c180def78b118a3
Ana Clara <ana4ew.br@gmail.com>
2025-01-28 21:31:15 -0300

    Created: Query to find patient by name

--- src/main/java/br/com/start/uni_clin/repositories/PatientRepository.java
+++ src/main/java/br/com/start/uni_clin/repositories/PatientRepository.java
@@ -1,11 +1,20 @@
 package br.com.start.uni_clin.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import br.com.start.uni_clin.entities.Doctor;
 import br.com.start.uni_clin.entities.Patient;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long>{
     
+    @NativeQuery("SELECT p.id, p.email, p.name, p.phone " +
+                    "FROM patient p " +
+                        "WHERE LOWER(p.name) LIKE LOWER(%:name%)")
+    List<Patient> findByName(@Param("name") String name);
 }
